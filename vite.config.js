69de5b52fafd4df5685363b5dc71c5482592ebbb
Akshit <f20220429@pilani.bits-pilani.ac.in>
2023-08-25 01:15:06 +0530

    add workflow

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-import pluginRewriteAll from 'vite-plugin-rewrite-all';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 
 // https://vitejs.dev/config/
-export default({
-  // plugins: [react()],
-  plugins: [pluginRewriteAll()]
+export default defineConfig({
+  plugins: [react()],
+  base: "/vite-react-router/"
 })
 
 
