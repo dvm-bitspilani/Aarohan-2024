b0e3027a3c673b8e6310a9bdf9c62f19832eea53
Akshit <f20220429@pilani.bits-pilani.ac.in>
2023-08-25 00:59:45 +0530

    fix: vite config issue

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 import pluginRewriteAll from 'vite-plugin-rewrite-all';
 
 
-
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default({
+  // plugins: [react()],
   plugins: [pluginRewriteAll()]
 })
 
