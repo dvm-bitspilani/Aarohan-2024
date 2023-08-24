381d18f9f40768454acf9a6bfb803a485989b0af
Akshit <f20220429@pilani.bits-pilani.ac.in>
2023-08-24 22:44:12 +0530

    build: add sample paper pdf

--- src/pages/Sample.jsx
+++ src/pages/Sample.jsx
@@ -5,6 +5,7 @@ import { Document, Page, pdfjs } from 'react-pdf'
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/legacy/build/pdf.worker.min.js`;
 import SamplePDF from '/public/document.pdf'
 import "../Styles/Sample.css"
+import { Link } from "react-router-dom";
 
 
 export default function Sample() {
@@ -14,8 +15,11 @@ export default function Sample() {
                 <>
                         <Navbar />
                         <div className="page">
-                                <div className="heading">Sample Paper</div>
-                                <div className="PDF-viewer">
+                                <div className="heading" style={{width: "100%", textAlign: "center", paddingTop: "2rem"}}>Sample Paper</div>
+                                <div className="pdf">
+                                <iframe src="https://drive.google.com/file/d/1epMPX3r90rrt6Sa0TEsWfDEG8XO5WCz4/preview" allow="autoplay"></iframe>
+
+                                <a href="https://drive.google.com/u/0/uc?id=1epMPX3r90rrt6Sa0TEsWfDEG8XO5WCz4&export=download"><button className="download">DOWNLOAD</button></a>
                                 </div>
                         </div>
 
