0b9db3ca03e3f441def37efaafb0f3c42ffa4092
Atharv Agarwal <atharv20agarwal@gmail.com>
2024-07-21 00:01:05 +0530

    feat: added test for total students = 0

--- src/Components/Forms/ValidationSchemas/formSchemas.js
+++ src/Components/Forms/ValidationSchemas/formSchemas.js
@@ -16,10 +16,15 @@ export const schoolFormSchema = Yup.object({
     school_poc_name: Yup.string().required("All fields are required"),
     city: Yup.string().required("All fields are required"),
     state: Yup.string().required("All fields are required"),
-    ninth_class_students: Yup.string().matches(/^\d+$/g, "Number of students has to be 0 or positive").required("All fields are required"),
-    tenth_class_students: Yup.string().matches(/^\d+$/g, "Number of students has to be 0 or positive").required("All fields are required"),
-    eleventh_class_students: Yup.string().matches(/^\d+$/g, "Number of students has to be 0 or positive").required("All fields are required"),
-    twelfth_class_students: Yup.string().matches(/^\d+$/g, "Number of students has to be 0 or positive").required("All fields are required"),
+    ninth_class_students: Yup.string().matches(/^\d+$/g, "Number of students has to be 0 or positive"),
+    tenth_class_students: Yup.string().matches(/^\d+$/g, "Number of students has to be 0 or positive"),
+    eleventh_class_students: Yup.string().matches(/^\d+$/g, "Number of students has to be 0 or positive"),
+    twelfth_class_students: Yup.string().matches(/^\d+$/g, "Number of students has to be 0 or positive"),
     contact_no: Yup.string().matches(/^(\+\d{1,2}\s?)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/g, 'Please enter a valid phone number').required('All fields are required'),
     email_id: Yup.string().email("Email is invalid").required("All fields are required")
-})
+}).test('students-zero', "Total number of students cannot be 0",
+    (values) => {
+        const tempArr = [+values.ninth_class_students, +values.tenth_class_students, +values.eleventh_class_students, +values.twelfth_class_students]
+        return tempArr.some(number => number !== 0)
+    }
+)
