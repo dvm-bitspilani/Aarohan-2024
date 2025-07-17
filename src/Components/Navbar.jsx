3dad4b5a3149b594215770aba8457bb0e64db1d7
Atharv Agarwal <atharv20agarwal@gmail.com>
2025-07-17 22:37:00 +0530

    Update event details for Aarohan 2026
    - Changed meta title and keywords in index.html to reflect the year 2026.
    - Updated Navbar component to use the new ApogeeLogo2026.svg and adjusted links for the current event.
    - Added new SVG logo for Apogee 2026 and removed the old PNG logo.

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,58 +1,87 @@
-import React, { useState, useEffect } from "react";
-import ApogeeLogo from '../images/apogeeLogo.png'
-import ApogeeLogo2025 from '../images/ApogeeLogo2025.svg'
-import { Link, useLocation } from "react-router-dom";
-import "../Styles/Hamburger.css"
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import ApogeeLogo2026 from "../images/ApogeeLogo2026.svg";
+import "../Styles/Hamburger.css";
 
 export default function Navbar(props) {
-    const [isHamOpen, setIsHamOpen] = useState(false)
+  const [isHamOpen, setIsHamOpen] = useState(false);
 
-    const handleHamOpen = () => {
-        setIsHamOpen(!isHamOpen)
-    }
+  const handleHamOpen = () => {
+    setIsHamOpen(!isHamOpen);
+  };
 
-    useEffect(() => {
-        isHamOpen ? document.body.style.overflowY = "hidden" : document.body.style.overflowY = "visible";
-    }, [isHamOpen])
+  useEffect(() => {
+    isHamOpen
+      ? (document.body.style.overflowY = "hidden")
+      : (document.body.style.overflowY = "visible");
+  }, [isHamOpen]);
 
-    return (
-        <>
-            <div className="navbar">
-                <div className="nav-container">
-                    <div className="nav-logo">
-                        <a href="https://www.bits-apogee.org/" style={{ textDecoration: "none" }}><img src={ApogeeLogo2025} alt="logo" draggable="false" /></a>
-                    </div>
-                    <div>
-                        <ul className="nav-list">
-                            <Link to={"/"} style={{ textDecoration: "none" }}><li>HOME</li></Link>
-                            <a href="/aarohan2025/#about" style={{ textDecoration: "none" }}><li>ABOUT US</li></a>
-                            <Link to={"/SamplePaper"} style={{ textDecoration: "none" }}><li>SAMPLE PAPER</li></Link>
-                            <Link to={"/FAQ"} style={{ textDecoration: "none" }}><li>FAQ</li></Link>
-                            <Link to={"/Gallery"} style={{ textDecoration: "none" }}><li>GALLERY</li></Link>
-                        </ul>
-                    </div>
+  return (
+    <>
+      <div className="navbar">
+        <div className="nav-container">
+          <div className="nav-logo">
+            <a
+              href="https://www.bits-apogee.org/"
+              style={{ textDecoration: "none" }}
+            >
+              <img src={ApogeeLogo2026} alt="logo" draggable="false" />
+            </a>
+          </div>
+          <div>
+            <ul className="nav-list">
+              <Link to={"/"} style={{ textDecoration: "none" }}>
+                <li>HOME</li>
+              </Link>
+              <a href="/aarohan2025/#about" style={{ textDecoration: "none" }}>
+                <li>ABOUT US</li>
+              </a>
+              <Link to={"/SamplePaper"} style={{ textDecoration: "none" }}>
+                <li>SAMPLE PAPER</li>
+              </Link>
+              <Link to={"/FAQ"} style={{ textDecoration: "none" }}>
+                <li>FAQ</li>
+              </Link>
+              <Link to={"/Gallery"} style={{ textDecoration: "none" }}>
+                <li>GALLERY</li>
+              </Link>
+            </ul>
+          </div>
 
+          <div className="row ham">
+            <input type="checkbox" id="hamburger" checked={isHamOpen} />
+            <label
+              htmlFor="hamburger"
+              className="hamburger"
+              onClick={handleHamOpen}
+            >
+              <span className="line"></span>
+              <span className="line"></span>
+              <span className="line"></span>
+            </label>
+          </div>
 
-                    <div className="row ham" >
-                        <input type="checkbox" id="hamburger" checked={isHamOpen} />
-                        <label htmlFor="hamburger" className="hamburger" onClick={handleHamOpen}>
-                            <span className="line"></span>
-                            <span className="line"></span>
-                            <span className="line"></span>
-                        </label>
-                    </div>
-
-                    <div className={`ham-menu ${isHamOpen && 'open'}`}>
-                        <ul className="ham-list">
-                            <Link to={"/"} style={{ textDecoration: "none" }}><li onClick={handleHamOpen}>HOME</li></Link>
-                            <a href="/aarohan2025/#about" style={{ textDecoration: "none" }}><li onClick={handleHamOpen}>ABOUT US</li></a>
-                            <Link to={"/SamplePaper"} style={{ textDecoration: "none" }}><li onClick={handleHamOpen}>SAMPLE PAPER</li></Link>
-                            <Link to={"/FAQ"} style={{ textDecoration: "none" }}><li onClick={handleHamOpen}>FAQ</li></Link>
-                            <Link to={"/Gallery"} style={{ textDecoration: "none" }}><li onClick={handleHamOpen}>GALLERY</li></Link>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+          <div className={`ham-menu ${isHamOpen && "open"}`}>
+            <ul className="ham-list">
+              <Link to={"/"} style={{ textDecoration: "none" }}>
+                <li onClick={handleHamOpen}>HOME</li>
+              </Link>
+              <a href="/aarohan2025/#about" style={{ textDecoration: "none" }}>
+                <li onClick={handleHamOpen}>ABOUT US</li>
+              </a>
+              <Link to={"/SamplePaper"} style={{ textDecoration: "none" }}>
+                <li onClick={handleHamOpen}>SAMPLE PAPER</li>
+              </Link>
+              <Link to={"/FAQ"} style={{ textDecoration: "none" }}>
+                <li onClick={handleHamOpen}>FAQ</li>
+              </Link>
+              <Link to={"/Gallery"} style={{ textDecoration: "none" }}>
+                <li onClick={handleHamOpen}>GALLERY</li>
+              </Link>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
