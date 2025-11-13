7a8ad0413b78d8539ed520244fa69d6bb18ea7c2
AnonymousTechDeveloper <anshmalviya54@gmail.com>
2025-11-13 20:33:02 +0530

    removed morning slot for nov23

--- src/Components/Information.jsx
+++ src/Components/Information.jsx
@@ -11,7 +11,7 @@ export default function Information({
     slots: ["9:00 a.m. - 11:00 a.m.", "3:00 p.m. - 5:00 p.m."]
   }, {
     date: "23rd November, 2025",
-    slots: ["9:00 a.m. - 11:00 a.m.", "3:00 p.m. - 5:00 p.m."]
+    slots: ["3:00 p.m. - 5:00 p.m."]
   }],
   lastDate = "20th November, 2025",
 }) {
--- src/Utils/formInformation.js
+++ src/Utils/formInformation.js
@@ -6,7 +6,7 @@ export const DATES = [{
   slots: ["9:00 a.m. - 11:00 a.m.", "3:00 p.m. - 5:00 p.m."]
 }, {
   date: "23rd November, 2025",
-  slots: ["9:00 a.m. - 11:00 a.m.", "3:00 p.m. - 5:00 p.m."]
+  slots: ["3:00 p.m. - 5:00 p.m."]
 }]
 
 export const LAST_DATE = "20th November, 2025";
