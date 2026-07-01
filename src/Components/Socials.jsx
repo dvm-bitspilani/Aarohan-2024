6dcb23dda849caf8c8a9c154b5ad6e8b32627e1e
Mystico-08 <gantavyamishra@gmail.com>
2026-07-02 01:10:01 +0530

    Socials section

--- src/Components/Socials.jsx
+++ src/Components/Socials.jsx
@@ -1,10 +1,33 @@
 import "../Styles/Socials.css";
 
-
 export default function Socials() {
     return (
-        <section>
-            <h2></h2>
+        <section className="socials-container">
+            <div className="socials-box">
+                <h2>Our Socials</h2>
+
+                <div className="socials-links">
+                    <a href="https://www.instagram.com/aarohan_bits.pilani/" target="_blank" rel="noopener noreferrer" className="socials-sites">
+                        <i className="fab fa-instagram"></i>
+                        <span>Instagram</span>
+                    </a>
+
+                    <a href="https://www.facebook.com/aarohan.bits/" target="_blank" rel="noopener noreferrer" className="socials-sites">
+                        <i className="fab fa-facebook-f"></i>
+                        <span>Facebook</span>
+                    </a>
+
+                    <a href="linkedin.com/company/aarohan-bits-pilani/" target="_blank" rel="noopener noreferrer" className="socials-sites">
+                        <i className="fab fa-linkedin-in"></i>
+                        <span>LinkedIn</span>
+                    </a>
+
+                    <div className="socials-sites gmail">
+                        <i className="fas fa-envelope"></i>
+                        <span>aarohan@bits-apogee.org</span>
+                    </div>
+                </div>
+            </div>
         </section>
     );
 }
